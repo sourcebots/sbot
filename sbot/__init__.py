76c61b678c0d9d7cd14d2899ee2923f652f761a1
Dan Trickey <dan@trickey.io>
2019-07-14 00:46:51 +0100

    Expose Motor Constants

--- sbot/__init__.py
+++ sbot/__init__.py
@@ -1,8 +1,18 @@
 """SourceBots API."""
 
+from j5.components.motor import MotorSpecialState
+
 from .logging import logger_setup
 from .robot import Robot, __version__
 
 logger_setup()
 
-__all__ = ["__version__", "Robot"]
+COAST = MotorSpecialState.COAST
+BRAKE = MotorSpecialState.BRAKE
+
+__all__ = [
+    "__version__",
+    "BRAKE",
+    "COAST",
+    "Robot",
+]
