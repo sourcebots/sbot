58cdd9215f0590814402607ad5a617dcbcc52d60
WillB97 <will@rudi.org.uk>
2023-06-04 22:35:47 +0100

    Implement timeout for posix

--- sbot/timeout.py
+++ sbot/timeout.py
@@ -1,22 +1,26 @@
-# """Code to kill robot after certain amount of time."""
-# import logging
-# from signal import SIGALRM, Signals, alarm, signal
-# from types import FrameType
+"""Code to kill robot after certain amount of time."""
+import logging
+import sys
+import signal
+from types import FrameType
 
-# LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
-
-# def timeout_handler(signal_type: Signals, stack_frame: FrameType) -> None:
-#     """Handle the `SIGALRM` to kill the current process."""
-#     raise SystemExit("Timeout expired: Game Over!")
+# TODO improve so will work on windows
 
 
-# def kill_after_delay(timeout_seconds: int) -> None:
-#     """Interrupts main process after the given delay."""
-#     LOGGER.debug(f"Kill Signal Timeout set: {timeout_seconds}s")
-#     signal(SIGALRM, timeout_handler)  # type: ignore
-#     alarm(timeout_seconds)
+def timeout_handler(signal_type: signal.Signals, stack_frame: FrameType) -> None:
+    """Handle the `SIGALRM` to kill the current process."""
+    raise SystemExit("Timeout expired: Game Over!")
+
 
 def kill_after_delay(timeout_seconds: int) -> None:
-    # TODO impliment so will work on windows
-    pass
+    """Interrupts main process after the given delay."""
+    if sys.platform == "win32":
+        logger.warning(
+            "Game timeout is not supported on Windows. "
+            "The code will not stop after the timeout.")
+    else:
+        logger.debug(f"Kill Signal Timeout set: {timeout_seconds}s")
+        signal.signal(signal.SIGALRM, timeout_handler)  # type: ignore
+        signal.alarm(timeout_seconds)
