9e3c0b33c005b0f73a5255dc10b70ca8a659e830
WillB97 <will@rudi.org.uk>
2023-06-25 22:00:33 +0100

    SP2023 specifics

--- sbot/game_specific.py
+++ sbot/game_specific.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 
 from typing import Iterable
 
-GAME_LENGTH = 120  # seconds
+GAME_LENGTH = 150  # seconds
 
 # Marker sizes are in mm
 MARKER_SIZES: dict[Iterable[int], int] = {
-    range(28): 200,  # 0 - 27 for arena boundary
+    range(28): 100,  # 0 - 27 for arena boundary
     range(28, 100): 80,  # Everything else is a token
 }
