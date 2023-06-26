9b1088d7fc40026276e0c98afdf24528ea98722f
WillB97 <will@rudi.org.uk>
2023-06-26 22:48:38 +0100

    Fix metadata test to use pytest environment manipulation

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,6 +1,5 @@
 """Test Metadata Loading."""
 
-from os import environ
 from pathlib import Path
 
 from pytest import raises
@@ -20,70 +19,71 @@ def test_metadata_key_error() -> None:
     assert str(e) == "Key \'beans\' not present in metadata"
 
 
-def test_load_no_env_var() -> None:
+def test_load_no_env_var(monkeypatch) -> None:
     """Test the behaviour when the environment variable is not set."""
+    monkeypatch.delenv(METADATA_ENV_VAR, raising=False)
     data = load()
     assert data == {"is_competition": False, "zone": 0}
 
 
-def test_load_file() -> None:
+def test_load_file(monkeypatch) -> None:
     """Test that we can load a file."""
     data_path = Path(__file__).parent / 'test_data/valid'
-    environ[METADATA_ENV_VAR] = str(data_path.absolute())
+    monkeypatch.setenv(METADATA_ENV_VAR, str(data_path.absolute()))
 
     data = load()
     assert data == {"is_competition": True, "zone": 1}
 
 
-def test_load_nested_file() -> None:
+def test_load_nested_file(monkeypatch) -> None:
     """Test that we can load a file from a nested directory."""
     data_path = Path(__file__).parent / 'test_data/nested'
-    environ[METADATA_ENV_VAR] = str(data_path.absolute())
+    monkeypatch.setenv(METADATA_ENV_VAR, str(data_path.absolute()))
 
     data = load()
     assert data == {"is_competition": True, "zone": 1}
 
 
-def test_load_file_not_found() -> None:
+def test_load_file_not_found(monkeypatch) -> None:
     """Test that the fallback data is loaded if no file is found."""
     data_path = Path(__file__).parent / "test_data/empty"
-    environ[METADATA_ENV_VAR] = str(data_path.absolute())
+    monkeypatch.setenv(METADATA_ENV_VAR, str(data_path.absolute()))
 
     data = load()
     assert data == {"is_competition": False, "zone": 0}
 
 
-def test_load_bad_file() -> None:
+def test_load_bad_file(monkeypatch) -> None:
     """Test that an exception is thrown when the JSON file is bad."""
     data_path = Path(__file__).parent / "test_data/bad"
-    environ[METADATA_ENV_VAR] = str(data_path.absolute())
+    monkeypatch.setenv(METADATA_ENV_VAR, str(data_path.absolute()))
 
     with raises(RuntimeError):
         load()
 
 
-def test_load_bad_data() -> None:
+def test_load_bad_data(monkeypatch) -> None:
     """Test that an exception is thrown when the JSON is not an object."""
     data_path = Path(__file__).parent / "test_data/not_object"
-    environ[METADATA_ENV_VAR] = str(data_path.absolute())
+    monkeypatch.setenv(METADATA_ENV_VAR, str(data_path.absolute()))
 
     with raises(TypeError):
         load()
 
 
-def test_load_bad_key() -> None:
+def test_load_bad_key(monkeypatch) -> None:
     """Test that an exception is thrown when the JSON has a missing key."""
     data_path = Path(__file__).parent / "test_data/missing_key"
-    environ[METADATA_ENV_VAR] = str(data_path.absolute())
+    monkeypatch.setenv(METADATA_ENV_VAR, str(data_path.absolute()))
 
     with raises(MetadataKeyError):
         load()
 
 
-def test_missing_metadata_path() -> None:
+def test_missing_metadata_path(monkeypatch) -> None:
     """Test that an exception is thrown when the metadata path does not exist."""
     data_path = Path(__file__).parent / "test_data/missing_path"
-    environ[METADATA_ENV_VAR] = str(data_path.absolute())
+    monkeypatch.setenv(METADATA_ENV_VAR, str(data_path.absolute()))
 
     with raises(FileNotFoundError):
         load()
