d46ec89c5cb98674a2cfd4f9798886f50e6fb145
WillB97 <will@rudi.org.uk>
2023-06-26 22:52:19 +0100

    Skip tests that don't run on windows

--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -1,4 +1,5 @@
 """Test the timeout function."""
+import sys
 from time import sleep
 
 import pytest
@@ -6,6 +7,7 @@ import pytest
 from sbot.timeout import kill_after_delay
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
 def test_kill_after_delay() -> None:
     """Test that a SystemExit is raised within the time."""
     with pytest.raises(SystemExit):
