baa3f67ef639cbea16f70fc9d82e55f2ca15ae31
WillB97 <will@rudi.org.uk>
2023-07-03 08:28:39 +0100

    Motor test script fixes

--- test_scripts/motor_test.py
+++ test_scripts/motor_test.py
@@ -13,6 +13,7 @@ import logging
 from time import sleep
 
 from sbot.motor_board import MotorBoard
+from sbot.power_board import PowerBoard, PowerOutputPosition
 from sbot.robot import setup_logging
 from sbot.utils import singular
 
@@ -24,6 +25,8 @@ logger = logging.getLogger("tester")
 
 def test_board():
     results = {}
+    pb = singular(PowerBoard._get_supported_boards())
+    pb.outputs[PowerOutputPosition.L2].is_enabled = True
     board = singular(MotorBoard._get_supported_boards())
     try:
         board_identity = board.identify()
@@ -37,7 +40,7 @@ def test_board():
         board.reset()
         sleep(2)
 
-        input_voltage = board.status()[2]
+        input_voltage = board.status()[1]
         # expected currents are calculated using this voltage
         logger.info(f"Detected input voltage {input_voltage:.3f}V")
         results['input_volt'] = input_voltage
@@ -47,7 +50,7 @@ def test_board():
         for motor in range(2):
             logger.info(f"Testing motor {motor}")
             # test off current
-            output_off_current = board.motors[motor].power
+            output_off_current = board.motors[motor].current
             logger.info(
                 f"Detected motor {motor} off state current: {output_off_current:.3f}A")
             results[f'motor_{motor}_off_current'] = output_off_current
@@ -56,14 +59,16 @@ def test_board():
                 "acceptable range of -0.2-0.2A.")
 
             for direction in (1, -1):
-                for abs_power in range(0, 1000, 100):
+                for abs_power in range(100, 10, -10):
                     power = abs_power * direction
-                    test_step = f"motor {motor}, {power / 10:.0f}% power"
-                    logger.info(f"Testing {power / 10:.0f}% power")
+                    test_step = f"motor {motor}, {power:.0f}% power"
+                    logger.info(f"Testing {power:.0f}% power")
+                    board.motors[motor].power = power / 100
+                    sleep(0.25)
 
-                    expected_out_current = (input_voltage / MOTOR_RESISTANCE) * (abs_power / 1000)  # noqa
-                    min_current_bound = (expected_out_current * 0.9)
-                    max_current_bound = (expected_out_current * 1.1)
+                    expected_out_current = (input_voltage / MOTOR_RESISTANCE) * (abs_power / 100)  # noqa
+                    min_current_bound = (expected_out_current * 0.9) - 0.2
+                    max_current_bound = (expected_out_current * 1.1) + 0.1
 
                     # test output current
                     output_current = board.motors[motor].current
@@ -71,7 +76,7 @@ def test_board():
                     results[f'motor_{motor}_{power}_current'] = output_current
                     assert min_current_bound < output_current < max_current_bound, (
                         f"{test_step}, current of {output_current:.3f}A is outside "
-                        f"acceptable range of {expected_out_current:.3f}A±10%.")
+                        f"acceptable range of {expected_out_current:.3f}A±10%-0.2A/+0.1A.")
 
         logger.info("Board passed")
     finally:
