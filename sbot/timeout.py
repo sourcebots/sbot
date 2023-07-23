5f3025c015c3b14f81d951f239145548de52ef89
Jake Howard <git@theorangeone.net>
2023-07-23 22:26:44 +0100

    Return successfully in the event of a timeout

--- sbot/timeout.py
+++ sbot/timeout.py
@@ -3,19 +3,32 @@ import logging
 import os
 import signal
 from threading import Timer
+from types import FrameType
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
 
-def timeout_handler() -> None:
+def raise_signal(signum: int) -> None:
+    """
+    Sends a signal to the current process.
+
+    Similar to `signal.raise_signal`, however uses the main process's signal handler.
+
+    :param signum: The signal to raise
+    """
+    os.kill(os.getpid(), signum)
+
+
+def timeout_handler(signal_type: int, stack_frame: Optional[FrameType]) -> None:
     """
     Handle the timeout to kill the current process.
 
     This function is called when the timeout expires and will stop the robot's main process.
     In order for this to work, any threads that are created must be daemons.
     """
-    logger.info("Timeout expired: Game Over!")
-    os.kill(os.getpid(), signal.SIGTERM)
+    print("Timeout expired: Game Over!")
+    exit(0)
 
 
 def kill_after_delay(timeout_seconds: int) -> None:
@@ -26,6 +39,10 @@ def kill_after_delay(timeout_seconds: int) -> None:
 
     :param timeout_seconds: The number of seconds to wait before killing the robot
     """
-    timer = Timer(timeout_seconds, timeout_handler)
+
+    signal.signal(signal.SIGTERM, timeout_handler)
+
+    timer = Timer(timeout_seconds, lambda: raise_signal(signal.SIGTERM))
+
     logger.debug(f"Kill Signal Timeout set: {timeout_seconds}s")
     timer.start()
--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -5,35 +5,29 @@ import os
 import signal
 import subprocess
 import sys
+import pytest
+
 
 from sbot.timeout import kill_after_delay
-from unittest.mock import Mock
 
 
 def test_kill_after_delay(monkeypatch) -> None:
     """Test that the process is killed within the time."""
-    kill = Mock()
-    pid = os.getpid()
-    monkeypatch.setattr(os, "kill", kill)
 
-    kill_after_delay(2)
-
-    sleep(1)
-    kill.assert_not_called()
-
-    sleep(1.5)  # Give the kernel a chance.
-    kill.assert_called_once_with(pid, signal.SIGTERM)
+    with pytest.raises(SystemExit):
+        kill_after_delay(2)
+        sleep(3)
 
 
 def test_kill_after_delay_e2e() -> None:
     child = subprocess.Popen([
         sys.executable,
         "-c",
-        'from sbot.timeout import kill_after_delay; import time; kill_after_delay(2); time.sleep(10)'
-    ])
+        'from sbot.timeout import kill_after_delay; import time; kill_after_delay(2); time.sleep(10)',
+    ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     sleep(1)
     assert child.poll() is None
 
     sleep(2)
-    assert abs(child.poll()) == signal.SIGTERM
+    assert child.poll() == 0
