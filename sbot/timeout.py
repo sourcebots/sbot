979369f2766485233ca429786c9e5781b333610e
Jake Howard <git@theorangeone.net>
2023-07-24 20:49:50 +0100

    Use SIGALRM on Unix, and a timer everywhere else
    This keeps the production implementation known-working and simple, and approximates it on Windows.
    
    This also has the benefit of ensuring a timeout yields a successful return code on the robot.

--- sbot/timeout.py
+++ sbot/timeout.py
@@ -1,30 +1,13 @@
 """Functions for killing the robot after a certain amount of time."""
 import logging
 import os
-import platform
 import signal
+import sys
 from threading import Timer
 from types import FrameType
 from typing import Optional
 
 logger = logging.getLogger(__name__)
-
-
-IS_LINUX = platform.system() == "Linux"
-
-# `SIGALRM` will only exist on Linux
-TERMINATING_SIGNAL = getattr(signal, "SIGALRM", signal.SIGTERM)
-
-
-def raise_signal(signum: int) -> None:
-    """
-    Sends a signal to the current process.
-
-    Similar to `signal.raise_signal`, however uses the main process's signal handler.
-
-    :param signum: The signal to raise
-    """
-    os.kill(os.getpid(), signum)
 
 
 def timeout_handler(signal_type: int, stack_frame: Optional[FrameType]) -> None:
@@ -33,9 +16,27 @@ def timeout_handler(signal_type: int, stack_frame: Optional[FrameType]) -> None:
 
     This function is called when the timeout expires and will stop the robot's main process.
     In order for this to work, any threads that are created must be daemons.
+
+    NOTE: This function is not called on Windows.
+
+    :param signal_type: The sginal that triggered this handler
+    :param stack_frame: The stack frame at the time of the signal
     """
-    print("Timeout expired: Game Over!")
+    logger.info("Timeout expired: Game Over!")
     exit(0)
+
+
+def win_timeout_handler() -> None:
+    """
+    Kill the main process on Windows.
+
+    This function is called when the timeout expires and will stop the robot's main process.
+    In order for this to work, any threads that are created must be daemons.
+
+    NOTE: This function is only called on Windows.
+    """
+    logger.info("Timeout expired: Game Over!")
+    os.kill(os.getpid(), signal.SIGTERM)
 
 
 def kill_after_delay(timeout_seconds: int) -> None:
@@ -47,10 +48,12 @@ def kill_after_delay(timeout_seconds: int) -> None:
     :param timeout_seconds: The number of seconds to wait before killing the robot
     """
 
-    if IS_LINUX:
-        signal.signal(TERMINATING_SIGNAL, timeout_handler)
-
-    timer = Timer(timeout_seconds, lambda: raise_signal(TERMINATING_SIGNAL))
-
+    if sys.platform == "win32":
+        # Windows doesn't have SIGALRM,
+        # so we approximate its functionality using a delayed SIGTERM
+        timer = Timer(timeout_seconds, win_timeout_handler)
+        timer.start()
+    else:
+        signal.signal(signal.SIGALRM, timeout_handler)
+        signal.alarm(timeout_seconds)
     logger.debug(f"Kill Signal Timeout set: {timeout_seconds}s")
-    timer.start()
--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -1,5 +1,5 @@
 """Test the timeout function."""
-from time import sleep
+from time import sleep, time
 
 import os
 import signal
@@ -20,14 +20,19 @@ def test_kill_after_delay(monkeypatch) -> None:
 
 
 def test_kill_after_delay_e2e() -> None:
+    start_time = time()
     child = subprocess.Popen([
         sys.executable,
         "-c",
         'from sbot.timeout import kill_after_delay; import time; kill_after_delay(2); time.sleep(10)',
     ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-    sleep(1)
-    assert child.poll() is None
+    child.wait(timeout=5)
 
-    sleep(2)
-    assert child.poll() == 0
+    assert time() - start_time == pytest.approx(2, rel=1)
+
+    if sys.platform == "win32":
+        # Windows terminates uncleanly
+        assert child.returncode == signal.SIGTERM
+    else:
+        assert child.returncode == 0
