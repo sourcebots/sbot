343fe4460a86ddb1320efebd13487e7e7359a554
WillB97 <will@rudi.org.uk>
2024-07-10 21:27:43 +0100

    Update power port for KCH usage

--- sbot/power_board.py
+++ sbot/power_board.py
@@ -61,7 +61,7 @@ class PowerStatus(NamedTuple):
 
 
 # This output is always on, and cannot be controlled via the API.
-BRAIN_OUTPUT = PowerOutputPosition.FIVE_VOLT
+BRAIN_OUTPUT = PowerOutputPosition.L2
 
 
 class PowerBoard(Board):
