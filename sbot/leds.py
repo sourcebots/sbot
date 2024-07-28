7354128f08f58ec55e8ddb6ab3068333e21d4fa1
WillB97 <will@rudi.org.uk>
2024-07-28 22:58:40 +0100

    Implement LEDs for the simulator

--- sbot/leds.py
+++ sbot/leds.py
@@ -2,18 +2,27 @@
 from __future__ import annotations
 
 import atexit
+import logging
 import warnings
 from enum import IntEnum, unique
 from types import MappingProxyType
 from typing import Literal, Mapping, NamedTuple, Optional
 
-from .utils import IN_SIMULATOR
+from .exceptions import BoardDisconnectionError, IncorrectBoardError
+from .serial_wrapper import SerialWrapper
+from .utils import IN_SIMULATOR, Board, BoardIdentity, get_simulator_boards
 
 try:
     import RPi.GPIO as GPIO  # isort: ignore
     HAS_HAT = True if not IN_SIMULATOR else False
 except ImportError:
     HAS_HAT = False
+
+
+logger = logging.getLogger(__name__)
+
+# Only used in the simulator
+BAUDRATE = 115200
 
 
 class RGBled(NamedTuple):
@@ -82,9 +91,16 @@ def get_user_leds() -> Mapping[Literal['A', 'B', 'C'], LED]:
             k: PhysicalLED(v) for k, v in RobotLEDs.user_leds().items()
         })
     elif IN_SIMULATOR:
-        return MappingProxyType({
-            k: SimulationLED(v) for k, v in RobotLEDs.user_leds().items()
-        })
+        led_server = LedServer.initialise()
+        if led_server is not None:
+            return MappingProxyType({
+                k: SimulationLED(v, led_server)
+                for v, k in enumerate(RobotLEDs.user_leds().keys())
+            })
+        else:
+            return MappingProxyType({
+                k: LED(v) for k, v in RobotLEDs.user_leds().items()
+            })
     else:
         return MappingProxyType({
             k: LED(v) for k, v in RobotLEDs.user_leds().items()
@@ -194,21 +210,112 @@ class PhysicalLED(LED):
         )
 
 
+class LedServer(Board):
+    """
+    LED control over a socket.
+
+    Used when running in the simulator to control the simulated LEDs.
+    """
+
+    @staticmethod
+    def get_board_type() -> str:
+        """
+        Return the type of the board.
+
+        :return: The literal string 'KCHv1B'.
+        """
+        return 'KCHv1B'
+
+    def __init__(
+        self,
+        serial_port: str,
+        initial_identity: BoardIdentity | None = None,
+    ) -> None:
+        if initial_identity is None:
+            initial_identity = BoardIdentity()
+        self._serial = SerialWrapper(
+            serial_port,
+            BAUDRATE,
+            identity=initial_identity,
+        )
+
+        self._identity = self.identify()
+        if self._identity.board_type != self.get_board_type():
+            raise IncorrectBoardError(self._identity.board_type, self.get_board_type())
+        self._serial.set_identity(self._identity)
+
+        # Reset the board to a known state
+        self._serial.write('*RESET')
+
+    @classmethod
+    def initialise(cls) -> 'LedServer' | None:
+        """Initialise the LED server using simulator discovery."""
+        # The filter here is the name of the emulated board in the simulator
+        boards = get_simulator_boards('KCHv1B')
+
+        if not boards:
+            return None
+
+        board_info = boards[0]
+
+        # Create board identity from the info given
+        initial_identity = BoardIdentity(
+            manufacturer='sbot_simulator',
+            board_type=board_info.type_str,
+            asset_tag=board_info.serial_number,
+        )
+
+        try:
+            board = cls(board_info.url, initial_identity)
+        except BoardDisconnectionError:
+            logger.warning(
+                f"Simulator specified LED board at port {board_info.url!r}, "
+                "could not be identified. Ignoring this device")
+            return None
+        except IncorrectBoardError as err:
+            logger.warning(
+                f"Board returned type {err.returned_type!r}, "
+                f"expected {err.expected_type!r}. Ignoring this device")
+            return None
+
+        return board
+
+    def identify(self) -> BoardIdentity:
+        """
+        Get the identity of the board.
+
+        :return: The identity of the board.
+        """
+        response = self._serial.query('*IDN?')
+        return BoardIdentity(*response.split(':'))
+
+    def set_leds(self, led_num: int, value: tuple[bool, bool, bool]) -> None:
+        """Set the colour of the LED."""
+        self._serial.write(f'LED:{led_num}:SET:{value[0]:d}:{value[1]:d}:{value[2]:d}')
+
+    def get_leds(self, led_num: int) -> tuple[bool, bool, bool]:
+        """Get the colour of the LED."""
+        response = self._serial.query(f'LED:{led_num}:GET?')
+        red, green, blue = response.split(':')
+        return bool(int(red)), bool(int(green)), bool(int(blue))
+
+
 class SimulationLED(LED):
     """
     User programmable LED.
 
     Used when running in the simulator to control the simulated LEDs.
     """
-    __slots__ = ('_led',)
+    __slots__ = ('_led_num', '_server')
 
-    def __init__(self, led: RGBled) -> None:
-        self._led = led
+    def __init__(self, led_num: int, server: LedServer) -> None:
+        self._led_num = led_num
+        self._server = server
 
     @property
     def colour(self) -> tuple[bool, bool, bool]:
         """Get the colour of the user LED."""
-        return False, False, False
+        return self._server.get_leds(self._led_num)
 
     @colour.setter
     def colour(self, value: tuple[bool, bool, bool]) -> None:
@@ -216,4 +323,11 @@ class SimulationLED(LED):
         if not isinstance(value, (tuple, list)) or len(value) != 3:
             raise ValueError("The LED requires 3 values for its colour")
 
-        pass
+        self._server.set_leds(
+            self._led_num,
+            (
+                bool(value[0]),
+                bool(value[1]),
+                bool(value[2]),
+            )
+        )
