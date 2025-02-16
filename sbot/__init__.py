d294d726d67de8259efc41f33270a37d43ce149b
WillB97 <will@rudi.org.uk>
2025-02-16 21:43:20 +0000

    Expose various enums

--- sbot/__init__.py
+++ sbot/__init__.py
@@ -1,6 +1,32 @@
 from os import environ as _environ
 
+from .arduinos import AnalogPin, GPIOPinMode
+from .leds import Colour
+from .motors import MotorPower
+from .power import PowerOutputPosition
+from .utils import Note
+
 if 'SBOT_PYTEST' not in _environ:
     from .robot import arduino, comp, leds, motors, power, servos, utils, vision
 
-__all__ = ['arduino', 'comp', 'leds', 'motors', 'power', 'servos', 'utils', 'vision']
+BRAKE = MotorPower.BRAKE
+COAST = MotorPower.COAST
+
+__all__ = [
+    'BRAKE',
+    'COAST',
+    'AnalogPin',
+    'Colour',
+    'GPIOPinMode',
+    'MotorPower',
+    'Note',
+    'PowerOutputPosition',
+    'arduino',
+    'comp',
+    'leds',
+    'motors',
+    'power',
+    'servos',
+    'utils',
+    'vision',
+]
